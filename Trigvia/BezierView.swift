abcb6fa4c27e08e92f6b46b0a8bb72e2b75529e1
Alumno <Aalumno@Laboratorios-MacBook-Pro-2.local>
2019-03-22 15:44:54 -0600

    Added triangle drawing

---
+++ Trigvia/BezierView.swift
@@ -0,0 +1,42 @@
+//
+//  BezierView.swift
+//  Trigvia
+//
+//  Created by Alumno on 3/22/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class BezierView: UIView {
+
+    var path: UIBezierPath!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        //    self.createRectangle()
+        self.createTriangle()
+        
+        // Specify a border (stroke) color.
+        UIColor.black.setStroke()
+        path.stroke()
+    }
+    
+    func createTriangle() {
+        path = UIBezierPath()
+        path.move(to: CGPoint(x: self.frame.width/2, y: 0.0))
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        path.close()
+    }
+
+}
--- Trigvia/PlaygroundViewController.swift
+++ Trigvia/PlaygroundViewController.swift
@@ -20,9 +20,21 @@ class PlaygroundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let width: CGFloat = 240.0
+        let height: CGFloat = 160.0
+        
+        let bezierView = BezierView(frame: CGRect(x: self.view.frame.size.width/2 - width/2,
+                                              y: self.view.frame.size.height/2 - height/2,
+                                              width: width,
+                                              height: height))
+        
+        self.view.addSubview(bezierView)
+    }
 
     /*
     // MARK: - Navigation
