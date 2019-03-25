8b8da70fece24596ebb77067b69f7d5f897580fa
Juan Pablo Galaz Chávez <juanpablogalazchavez@Juans-MacBook-Pro-4.local>
2019-03-25 14:09:47 -0600

    Quitar botones

--- Trigvia/PlaygroundViewController.swift
+++ Trigvia/PlaygroundViewController.swift
@@ -17,6 +17,15 @@ class PlaygroundViewController: UIViewController {
     var dSide2 : Double = 40.0
     var dSide3 : Double = 50.0
     
+    var x1: Double!
+    var y1: Double!
+    var x2: Double!
+    var y2: Double!
+    var x3: Double!
+    var y3: Double!
+    
+    @IBOutlet var bezierView: BezierView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,48 +33,49 @@ class PlaygroundViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        x1 = 0.0
+        y1 = 60.0
+        x2 = dSide1
+        y2 = y1
+        x3 = cos(dAngle2 / 360 * 2 * Double.pi) * dSide2
+        y3 = sin(dAngle2 / 360 * 2 * Double.pi) * dSide2
         drawTriangle()
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let error = 10.0
+        let touch = touches.first!
+        let location = touch.location(in: bezierView)
+        let hitView = bezierView.hitTest(location, with: event)
+        if (bezierView == hitView){
+            let yFinal = Double(location.y)
+            let xFinal = Double(location.x)
+            let dist1 = hypot(x1 - xFinal, y1 - yFinal)
+            let dist2 = hypot(x2 - xFinal, y2 - yFinal)
+            let dist3 = hypot(x3 - xFinal, y3 - yFinal)
+            if dist1 < error {
+                x1 = xFinal
+                y1 = yFinal
+                drawTriangle()
+            }else if  dist2 < error{
+                x2 = xFinal
+                y2 = yFinal
+                drawTriangle()
+            }else if  dist3 < error{
+                x3 = xFinal
+                y3 = yFinal
+                drawTriangle()
+            }
+        }
     }
     
     func drawTriangle() {
-        let width: CGFloat = 240.0
-        let height: CGFloat = 160.0
         
-        let bezierView = BezierView(frame: CGRect(x: self.view.frame.size.width / 2 - width / 2,
-                                                  y: self.view.frame.size.height / 2 - height / 2,
-                                                  width: width,
-                                                  height: height))
-        
-        let x1 = 0.0
-        let y1 = 60.0
-        let x2 = dSide1
-        let y2 = y1
-        let x3 = cos(dAngle2 / 360 * 2 * Double.pi) * dSide2
-        let y3 = sin(dAngle2 / 360 * 2 * Double.pi) * dSide2
-        
+        bezierView = BezierView(frame: bezierView.frame)
         bezierView.setCoordinates(_x1: x1, _y1: y1,
                                   _x2: x2, _y2: y2,
                                   _x3: x3, _y3: y3)
         self.view.addSubview(bezierView)
-    }
-
-    @IBAction func arriba(_ sender: UILongPressGestureRecognizer) {
-        dSide2 = min(200, dSide2 + 1)
-        drawTriangle()
-    }
-    
-    @IBAction func abajo(_ sender: UILongPressGestureRecognizer) {
-        dSide2 = max(40, dSide2 - 1)
-        drawTriangle()
-    }
-    @IBAction func izq(_ sender: UILongPressGestureRecognizer) {
-        dSide1 = max(30, dSide1 - 1)
-        drawTriangle()
-    }
-    @IBAction func der(_ sender: UILongPressGestureRecognizer) {
-        dSide1 = min(200, dSide1 + 1)
-        drawTriangle()
     }
     
     /*
